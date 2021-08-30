50b888916b76eaec60a49fbc4dfa7dca5ca5d052
Higor braga <higorbraga00@gmail.com>
2021-08-30 13:06:25 -0300

    pattern Chain of Responsibility

---
+++ src/br/com/alura/loja/TestesDescontos.java
@@ -0,0 +1,19 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+import br.com.alura.loja.imposto.CaculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TestesDescontos {
+
+    public static void main(String[] args) {
+        Orcamento primeiroOrcamento = new Orcamento(new BigDecimal("200"), 6);
+        Orcamento segundoOrcamento = new Orcamento(new BigDecimal("1000"), 1);
+        CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
+        System.out.println(calculadoraDeDescontos.calcular(primeiroOrcamento));
+        System.out.println(calculadoraDeDescontos.calcular(segundoOrcamento));
+    }
+}
